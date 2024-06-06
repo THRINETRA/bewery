1bf1b237bea05e59186d9af84b3fb5ab8842b3e3
thrinetra <thrinetravikram@gmail.com>
2024-06-06 20:14:14 +0530

    change2

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -1,19 +1,16 @@
 "use client";
-import { ReactNode, useState } from "react";
-import Footer from "@/containers/Footer/page";
-import Navbar from "@/containers/Navbar/page";
-import Image from "next/image";
+import React, { ReactNode, useState } from "react";
 import MainTemplate from "@/template";
 import Breweries, { Brewery } from "@/containers/Maincontent/page";
 
-interface Props {
-  children: ReactNode;
-}
+// interface Props {
+//   children?: ReactNode;
+// }
 
-const Mainrender: React.FC<Props> = ({ children }: { children: any }) => {
+const Mainrender: React.FC = () => {
   const [breweries, setBreweries] = useState<Brewery[]>([]);
+
   const filterData = (name: string) => {
-    console.log(name);
     const filteredData = breweries.filter((brewerie) => {
       return (
         brewerie.name.toLowerCase().includes(name.toLowerCase()) ||
@@ -21,19 +18,14 @@ const Mainrender: React.FC<Props> = ({ children }: { children: any }) => {
         brewerie.brewery_type.toLowerCase().includes(name.toLowerCase())
       );
     });
-    console.log(filteredData, "data");
     setBreweries(filteredData);
     return filteredData;
   };
+
   return (
-    <MainTemplate
-      children={
-        <>
-          <Breweries setBreweries={setBreweries} breweries={breweries} />
-        </>
-      }
-      filterData={filterData}
-    />
+    <MainTemplate filterData={filterData}>
+      <Breweries setBreweries={setBreweries} breweries={breweries} />
+    </MainTemplate>
   );
 };
 
